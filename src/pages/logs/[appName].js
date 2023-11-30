a48e96d8f066b6c13b895e95119a288f10ac7e75
Nickygee <thenickygee@proton.me>
2023-11-29 20:25:31 -0700

    fix: pm2 app data populating on log page

--- src/pages/logs/[appName].js
+++ src/pages/logs/[appName].js
@@ -20,37 +20,38 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { toast } from 'react-toastify';
 
-const LogsPage = (app) => {
+const LogsPage = () => {
   const router = useRouter();
   const { appName, index } = router.query;
-
   const [logs, setLogs] = useState({ out: [], error: [] });
   const [loading, setLoading] = useState(false);
-  const [apps, setApps] = useState([]);
   const [currentAppData, setCurrentAppData] = useState(null);
-  const [groupedApps, setGroupedApps] = useState({});
+  const [appsData, setAppsData] = useState([]);
 
   useEffect(() => {
     async function fetchApps() {
-      const res = await fetch('/api/pm2/apps');
-      const data = await res.json();
-
-      const grouped = data.reduce((acc, app) => {
-        if (!acc[app.name]) acc[app.name] = [];
-        acc[app.name].push(app);
-        return acc;
-      }, {});
-      setApps(data);
-      setGroupedApps(grouped);
+      try {
+        const response = await fetch('/api/pm2/apps');
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setAppsData(data); // Set the fetched data
+      } catch (error) {
+        console.error('Failed to fetch data:', error);
+        // Handle error
+      }
     }
 
     fetchApps();
+  }, []); // Empty dependency array means this runs once when the component mounts
 
-    const intervalId = setInterval(() => {
-      fetchApps();
-    }, 5000);
-    return () => clearInterval(intervalId);
-  }, []);
+  useEffect(() => {
+    if (appsData && appName) {
+      const filteredApp = appsData.find((app) => app.name === appName);
+      setCurrentAppData(filteredApp);
+    }
+  }, [appsData, appName]);
 
   const pm2AppAction = async (appName, action) => {
     try {
@@ -73,12 +74,6 @@ const LogsPage = (app) => {
       toast.error('Error performing action on app');
     }
   };
-
-  useEffect(() => {
-    if (appName && groupedApps[appName] && groupedApps[appName].length > 0) {
-      setCurrentAppData(groupedApps[appName][0]);
-    }
-  }, [appName, groupedApps]);
 
   useEffect(() => {
     if (appName) {
@@ -123,12 +118,6 @@ const LogsPage = (app) => {
     return 'text-red-500';
   }
 
-  useEffect(() => {
-    if (appName && groupedApps[appName]) {
-      setCurrentAppData(groupedApps[appName]);
-    }
-  }, [appName, groupedApps]);
-
   return (
     <>
       <Navbar />
@@ -139,202 +128,213 @@ const LogsPage = (app) => {
             DASHBOARD
           </button>
         </Link>
-        {app.status}
-        <div
-          key={app.instanceId}
-          className='flex flex-col rounded-md shadow-xl min-w-max max-w-content bg-zinc-950 p-2 py-0 pb-2 w-full max-w-5xl mb-4'
-        >
-          <div className='flex flex-col'>
-            <div className='flex items-center justify-between gap-6 p-2 mb-2 rounded-md'>
-              <p className='flex items-center gap-2 text-xl font-bold text-gray-100'>
-                {' '}
-                <span className='font-bold text-zinc-300'>
-                  Instance [ {app.instanceId} ]
+        {currentAppData && (
+          <div
+            key={currentAppData.instanceId}
+            className='flex flex-col rounded-md shadow-xl min-w-max max-w-content bg-zinc-950 p-2 py-0 pb-2 w-full max-w-5xl mb-4'
+          >
+            <div className='flex flex-col'>
+              <div className='flex items-center justify-between gap-6 p-2 mb-2 rounded-md'>
+                <p className='flex items-center gap-2 text-xl font-bold text-gray-100'>
+                  {' '}
+                  <span className='font-bold text-zinc-300'>
+                    Instance [ {currentAppData.instanceId} ]
+                  </span>
+                </p>
+                <span
+                  className={`badge ${
+                    currentAppData.status === 'online'
+                      ? 'bg-green-800 text-white px-2 py-0.5 rounded-md'
+                      : 'bg-red-800 text-white px-2 py-0.5 rounded-md'
+                  }`}
+                >
+                  {currentAppData.status}
                 </span>
-              </p>
-              <span
-                className={`badge ${
-                  app.status === 'online'
-                    ? 'bg-green-800 text-white px-2 py-0.5 rounded-md'
-                    : 'bg-red-800 text-white px-2 py-0.5 rounded-md'
-                }`}
-              >
-                {app.status}
-              </span>
-            </div>
+              </div>
 
-            <div className='flex flex-col pb-4'>
-              <p
-                className={`${
-                  index === 1 ? 'md:flex-row-reverse' : 'flex'
-                } flex items-center gap-2 w-full justify-between`}
-              >
-                <span
+              <div className='flex flex-col pb-4'>
+                <p
                   className={`${
                     index === 1 ? 'md:flex-row-reverse' : 'flex'
-                  } flex gap-2`}
+                  } flex items-center gap-2 w-full justify-between`}
                 >
-                  {' '}
-                  <FontAwesomeIcon
-                    icon={faMicrochip}
-                    className={`h-5 ${getColorClass(app.cpu, {
-                      green: 50,
-                      yellow: 70,
-                      orange: 85,
-                    })}`}
-                  />
-                  <span className='text-zinc-400'>CPU</span>
-                </span>
-                <span className='font-bold text-zinc-100 text-xl'>
-                  {app.cpu}%
-                </span>
-              </p>
-              <p
-                className={`${
-                  index === 1 ? 'md:flex-row-reverse' : 'flex'
-                } flex items-center gap-2 w-full justify-between`}
-              >
-                {' '}
-                <span
-                  className={`${
-                    index === 1 ? 'md:flex-row-reverse' : 'flex'
-                  } flex gap-2`}
-                >
-                  <FontAwesomeIcon
-                    icon={faMemory}
-                    className={`h-5 ${getColorClass(app.memory, {
-                      green: 50000000,
-                      yellow: 10000000,
-                      orange: 150000000,
-                    })}`}
-                  />
-                  <span className='text-zinc-400'>MEM</span>
-                </span>
-                <span className='text-xl font-bold text-gray-100'>
-                  {(app.memory / (1024 * 1024)).toFixed(2) || '0'} MB
-                </span>
-              </p>
-              <p
-                className={`${
-                  index === 1 ? 'md:flex-row-reverse' : 'flex'
-                } flex items-center gap-2 w-full justify-between`}
-              >
-                {' '}
-                <span
-                  className={`${
-                    index === 1 ? 'md:flex-row-reverse' : 'flex'
-                  } flex gap-2`}
-                >
-                  {' '}
-                  <FontAwesomeIcon
-                    icon={faClock}
-                    className='h-5 text-amber-50'
-                  />
-                  <span className='text-zinc-400'>UPTIME</span>
-                </span>
-                <span className='font-bold text-zinc-100 text-xl'>
-                  {app.uptime || 'n/a'}
-                </span>
-              </p>
-              <p
-                className={`${
-                  index === 1 ? 'md:flex-row-reverse' : 'flex'
-                } flex items-center gap-2 w-full justify-between`}
-              >
-                {' '}
-                <span
-                  className={`${
-                    index === 1 ? 'md:flex-row-reverse' : 'flex'
-                  } flex gap-2`}
-                >
-                  {' '}
-                  <FontAwesomeIcon
-                    icon={faIdCard}
-                    className='h-5 text-amber-50'
-                  />
-                  <span className='text-zinc-400'>PID</span>
-                </span>
-                <span className='font-bold text-zinc-100'>
-                  {app.pid || 'n/a'}
-                </span>
-              </p>
-            </div>
-          </div>
-          <div className='flex flex-col gap-2'>
-            {/* Conditional rendering for buttons based on app status */}
-            {app.status === 'online' ? (
-              <>
-                <Popover className='relative flex w-full justify-around gap-2 text-zinc-100'>
-                  <Popover.Button className='bg-zinc-900 hover:border-zinc-400 border-zinc-800 border p-3 rounded-md flex items-center gap-2 text-zinc-100'>
+                  <span
+                    className={`${
+                      index === 1 ? 'md:flex-row-reverse' : 'flex'
+                    } flex gap-2`}
+                  >
+                    {' '}
                     <FontAwesomeIcon
-                      icon={faCog}
+                      icon={faMicrochip}
+                      className={`h-5 ${getColorClass(currentAppData.cpu, {
+                        green: 50,
+                        yellow: 70,
+                        orange: 85,
+                      })}`}
+                    />
+                    <span className='text-zinc-400'>CPU</span>
+                  </span>
+                  <span className='font-bold text-zinc-100'>
+                    {currentAppData.cpu}%
+                  </span>
+                </p>
+                <p
+                  className={`${
+                    index === 1 ? 'md:flex-row-reverse' : 'flex'
+                  } flex items-center gap-2 w-full justify-between`}
+                >
+                  {' '}
+                  <span
+                    className={`${
+                      index === 1 ? 'md:flex-row-reverse' : 'flex'
+                    } flex gap-2`}
+                  >
+                    <FontAwesomeIcon
+                      icon={faMemory}
+                      className={`h-5 ${getColorClass(currentAppData.memory, {
+                        green: 50000000,
+                        yellow: 10000000,
+                        orange: 150000000,
+                      })}`}
+                    />
+                    <span className='text-zinc-400'>MEM</span>
+                  </span>
+                  <span className='font-bold text-gray-100'>
+                    {(currentAppData.memory / (1024 * 1024)).toFixed(2) || '0'}{' '}
+                    MB
+                  </span>
+                </p>
+                <p
+                  className={`${
+                    index === 1 ? 'md:flex-row-reverse' : 'flex'
+                  } flex items-center gap-2 w-full justify-between`}
+                >
+                  {' '}
+                  <span
+                    className={`${
+                      index === 1 ? 'md:flex-row-reverse' : 'flex'
+                    } flex gap-2`}
+                  >
+                    {' '}
+                    <FontAwesomeIcon
+                      icon={faClock}
                       className='h-5 text-amber-50'
                     />
-                    Actions
-                  </Popover.Button>
-                  <Link href={`/logs/${encodeURIComponent(app.name)}`}>
-                    <button className='bg-zinc-900 hover:border-zinc-400 border-zinc-800 border rounded-md py-3 px-6 text-zinc-100'>
+                    <span className='text-zinc-400'>UPTIME</span>
+                  </span>
+                  <span className='font-bold text-zinc-100'>
+                    {currentAppData.uptime || 'n/a'}
+                  </span>
+                </p>
+                <p
+                  className={`${
+                    index === 1 ? 'md:flex-row-reverse' : 'flex'
+                  } flex items-center gap-2 w-full justify-between`}
+                >
+                  {' '}
+                  <span
+                    className={`${
+                      index === 1 ? 'md:flex-row-reverse' : 'flex'
+                    } flex gap-2`}
+                  >
+                    {' '}
+                    <FontAwesomeIcon
+                      icon={faIdCard}
+                      className='h-5 text-amber-50'
+                    />
+                    <span className='text-zinc-400'>PID</span>
+                  </span>
+                  <span className='font-bold text-zinc-100'>
+                    {currentAppData.pid || 'n/a'}
+                  </span>
+                </p>
+              </div>
+            </div>
+            <div className='flex flex-col gap-2'>
+              {/* Conditional rendering for buttons based on app status */}
+              {currentAppData.status === 'online' ? (
+                <>
+                  <Popover className='relative flex w-full justify-around gap-2 text-zinc-100'>
+                    <Popover.Button className='bg-zinc-900 w-full text-center justify-center hover:border-zinc-400 border-zinc-800 border p-3 rounded-md flex items-center gap-2 text-zinc-100'>
                       <FontAwesomeIcon
-                        icon={faTerminal}
-                        className=' text-amber-50'
-                      />{' '}
-                      <p>Logs</p>
-                    </button>
-                  </Link>
-                  <Popover.Panel className='absolute z-10 -mt-36 shadow-2xl'>
-                    <div className='flex flex-col gap-2 bg-zinc-700 p-3 rounded-md'>
-                      <button
-                        className='flex items-center justify-center gap-2 p-1 px-6 bg-zinc-800 rounded-md hover:bg-zinc-900 text-zinc-100'
-                        aria-label='Reload'
-                        onClick={() => pm2AppAction(app.name, 'reload')}
-                      >
+                        icon={faCog}
+                        className='h-5 text-amber-50'
+                      />
+                      Actions
+                    </Popover.Button>
+                    {/* <Link
+                      href={`/logs/${encodeURIComponent(currentAppData.name)}`}
+                    >
+                      <button className='bg-zinc-900 hover:border-zinc-400 border-zinc-800 border rounded-md py-3 px-6 text-zinc-100'>
                         <FontAwesomeIcon
-                          icon={faSyncAlt}
-                          className='h-5 text-amber-50'
-                        />
-                        Reload
+                          icon={faTerminal}
+                          className=' text-amber-50'
+                        />{' '}
+                        <p>Logs</p>
                       </button>
-                      <button
-                        className='flex items-center justify-center gap-2 p-1 px-3 bg-zinc-800 rounded-md hover:bg-zinc-900 text-zinc-100'
-                        aria-label='Restart'
-                        onClick={() => pm2AppAction(app.name, 'restart')}
-                      >
-                        <FontAwesomeIcon
-                          icon={faRecycle}
-                          className='h-5 text-white'
-                        />
-                        Restart
-                      </button>
-                      <button
-                        className='flex items-center justify-center gap-2 p-1 px-3 bg-zinc-800 rounded-md hover:bg-red-900 text-zinc-100'
-                        aria-label='Stop'
-                        onClick={() => pm2AppAction(app.name, 'stop')}
-                      >
-                        <FontAwesomeIcon
-                          icon={faStop}
-                          className='h-5 text-white'
-                        />
-                        Stop
-                      </button>
-                    </div>
-                  </Popover.Panel>
-                </Popover>
-              </>
-            ) : (
-              <button
-                className='flex items-center justify-center gap-2 py-3 bg-green-600 rounded-md hover:bg-green-700 text-zinc-100'
-                aria-label='Start'
-                onClick={() => pm2AppAction(app.name, 'start')}
-              >
-                <FontAwesomeIcon
-                  icon={faPlayCircle}
-                  className='h-5 text-amber-50'
-                />
-                Start
-              </button>
-            )}
+                    </Link> */}
+                    <Popover.Panel className='absolute z-10 -mt-36 shadow-2xl'>
+                      <div className='flex flex-col gap-2 bg-zinc-700 p-3 rounded-md'>
+                        <button
+                          className='flex items-center justify-center gap-2 p-1 px-6 bg-zinc-800 rounded-md hover:bg-zinc-900 text-zinc-100'
+                          aria-label='Reload'
+                          onClick={() =>
+                            pm2AppAction(currentAppData.name, 'reload')
+                          }
+                        >
+                          <FontAwesomeIcon
+                            icon={faSyncAlt}
+                            className='h-5 text-amber-50'
+                          />
+                          Reload
+                        </button>
+                        <button
+                          className='flex items-center justify-center gap-2 p-1 px-3 bg-zinc-800 rounded-md hover:bg-zinc-900 text-zinc-100'
+                          aria-label='Restart'
+                          onClick={() =>
+                            pm2AppAction(currentAppData.name, 'restart')
+                          }
+                        >
+                          <FontAwesomeIcon
+                            icon={faRecycle}
+                            className='h-5 text-white'
+                          />
+                          Restart
+                        </button>
+                        <button
+                          className='flex items-center justify-center gap-2 p-1 px-3 bg-zinc-800 rounded-md hover:bg-red-900 text-zinc-100'
+                          aria-label='Stop'
+                          onClick={() =>
+                            pm2AppAction(currentAppData.name, 'stop')
+                          }
+                        >
+                          <FontAwesomeIcon
+                            icon={faStop}
+                            className='h-5 text-white'
+                          />
+                          Stop
+                        </button>
+                      </div>
+                    </Popover.Panel>
+                  </Popover>
+                </>
+              ) : (
+                <button
+                  className='flex items-center justify-center gap-2 py-3 bg-green-600 rounded-md hover:bg-green-700 text-zinc-100'
+                  aria-label='Start'
+                  onClick={() => pm2AppAction(currentAppData.name, 'start')}
+                >
+                  <FontAwesomeIcon
+                    icon={faPlayCircle}
+                    className='h-5 text-amber-50'
+                  />
+                  Start
+                </button>
+              )}
+            </div>
           </div>
-        </div>
+        )}
+
         <h1 className='text-2xl font-bold mb-4 text-gray-100'>
           Logs for {appName}
         </h1>
