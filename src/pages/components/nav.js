1b41b9c483a09bf8b4ed624019689ac3161be5fc
Nickygee <thenickygee@proton.me>
2023-11-30 16:43:22 -0700

    fix: mobile nav layout adjustments

--- src/pages/components/nav.js
+++ src/pages/components/nav.js
@@ -16,11 +16,15 @@ import { faDochub, faGithub } from '@fortawesome/free-brands-svg-icons';
 const Navbar = () => {
   const { data: session } = useSession();
   return (
-    <div className='px-3 w-full z-50 fixed top-0 bg-black/80 backdrop-blur-sm h-[55px] justify-between items-center align-middle flex select-none cursor-default'>
+    <div className='px-3 w-full z-[100] fixed top-0 bg-black/80 backdrop-blur-sm h-[55px] justify-between items-center align-middle flex select-none cursor-default'>
       {' '}
-      <p className='font-bold text-gray-100 text-xl'>
+      <p className='font-bold text-gray-100 text-sm md:text-xl'>
         {' '}
-        <FontAwesomeIcon icon={faHeartPulse} className='pr-2' /> PM2 UI
+        <FontAwesomeIcon
+          icon={faHeartPulse}
+          className='pr-2 hidden md:relative'
+        />{' '}
+        PM2 UI
       </p>{' '}
       <div className={{ session } ? 'flex gap-2' : 'hidden'}>
         <button
@@ -32,13 +36,21 @@ const Navbar = () => {
           }
           className='p-1 px-3 flex h-min items-center font-bold text-white rounded-md bg-gradient-to-r from-zinc-600 to-zinc-700 hover:bg-blue-300'
         >
-          <FontAwesomeIcon icon={faBookBookmark} className='pr-2' /> PM2
+          <FontAwesomeIcon
+            icon={faBookBookmark}
+            className='pr-2 hidden md:relative'
+          />{' '}
+          PM2
         </button>
         <button
           onClick={() => window.open('https://www.dev.azure.com', '_blank')}
           className='p-1 px-3 flex h-min items-center font-bold text-white rounded-md bg-gradient-to-r from-zinc-600 to-zinc-700 hover:bg-blue-300'
         >
-          <FontAwesomeIcon icon={faWaveSquare} className='pr-2' /> AZURE
+          <FontAwesomeIcon
+            icon={faWaveSquare}
+            className='pr-2 hidden md:relative'
+          />{' '}
+          AZURE
         </button>
         <button
           onClick={() =>
@@ -46,7 +58,11 @@ const Navbar = () => {
           }
           className='p-1 px-3 flex h-min items-center font-bold text-white rounded-md bg-gradient-to-r from-zinc-600 to-zinc-700   hover:bg-blue-300'
         >
-          <FontAwesomeIcon icon={faGithub} className='pr-2' /> GITHUB
+          <FontAwesomeIcon
+            icon={faGithub}
+            className='pr-2 hidden md:relative'
+          />{' '}
+          GITHUB
         </button>
         {/* <AuthButton /> */}
       </div>
